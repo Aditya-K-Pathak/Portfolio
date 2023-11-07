96c8227d5f1d382f39a1d23efa71581722aeb0a0
Aditya-K-Pathak <adityapathak2874@ggmail.com>
2023-11-08 00:52:56 +0530

    V.0.1.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# Portfolio
+This Repository contains my portfolio built using Django.
+
+The portfolio contains files like settings, urls, wsgi etc.
+In order to host the project over internet a user need to so slight modification in the code by adding the name of the host link in settings.py.
+
+Requirement: Django
+Installation Command - pip install django
+
+To run the code on localhost a user need to be in the folder where manage.py is situated. In the same directory execute the code below
+python3 manage.py runserver
+
+Composition - 
+./static/ - contains image and css for the website i.e., static files.
+./templates/ - contains html file for redirecting urls to specific webpage.
---
+++ portfolio/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+import json
+
+class GetData:
+    def __init__(self) -> None:
+        with open("static/data/data.json", "r") as file:
+            self.data = file.read()
+            self.data = json.loads(self.data)
+            self.update_visitors()
+
+    def update_visitors(self) -> bool:
+        self.data["Visitors"] += 1
+        with open("static/data/data.json", "w") as file:
+            json.dump(self.data, file, indent=4)
+        return True
+
+    def get_home(self) -> dict:
+        self.update_visitors()
+        return dict(self.data)
+    
+    def get_patents(self) -> dict:
+        self.update_visitors()
+        return dict(self.data)
+    
+    def get_awards(self) -> dict:
+        self.update_visitors()
+        return dict(self.data)
+    
+    def get_projects(self) -> dict:
+        self.update_visitors()
+        return dict(self.data)
+    
+    def get_research(self) -> dict:
+        self.update_visitors()
+        return dict(self.data)
+
+webdata = GetData()
+def index(request):
+    return render(request, "index.html", webdata.get_home())
+
+def projects(requests):
+    return render(requests, "projects.html", webdata.get_projects())
+
+def patents(requests):
+    return render(requests, "patents.html", webdata.get_patents())
+
+def research(requests):
+    return render(requests, "research.html", webdata.get_research())
+
+def awards(requests):
+    return render(requests, "awards.html", webdata.get_awards())
